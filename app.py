6d3a7f328a5485cf4b1dd5f0be06a184c64e4532
argideritzalpea <crh2ke@virginia.edu>
2017-05-14 16:31:38 -0400

    Update app.py

--- app.py
+++ app.py
@@ -11,10 +11,7 @@ from pymongo import MongoClient
 
 # Flask app should start in global layout
 app = Flask(__name__)
-client = MongoClient('mongodb://***REMOVED***:***REMOVED***@cluster0-shard-00-00-b4hqz.mongodb.net:27017,cluster0-shard-00-01-b4hqz.mongodb.net:27017,cluster0-shard-00-02-b4hqz.mongodb.net:27017/chatbot?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
 
-col = client.chatbot.factbook
-print(col)
 
 
 @app.route('/webhook', methods=['POST'])
@@ -40,8 +37,7 @@ def makeWebhookResult(req):
     parameters = result.get("parameters")
     zone = parameters.get("country")
 
-    speech = "The cost of shipping to " + zone + " is " + col.distinct(zone)[0]['airports.txt'] + " euros."
-    speech = 0
+    speech = "The cost of shipping to " + zone + " is " + "5" + " euros."
 
     print("Response:")
     print(speech)
