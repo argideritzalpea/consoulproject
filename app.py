73502ac904294d59d80280117fe509419eb16e10
argideritzalpea <crh2ke@virginia.edu>
2017-05-14 16:47:01 -0400

    Update app.py

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ app = Flask(__name__)
 @app.route('/')
 def connect():
     client = MongoClient('mongodb://RadMajik:YUVBnmio5%@cluster0-shard-00-00-b4hqz.mongodb.net:27017,cluster0-shard-00-01-b4hqz.mongodb.net:27017,cluster0-shard-00-02-b4hqz.mongodb.net:27017/chatbot?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin')
-    col = client.chatbot.factbook
+    col = client.chatbot
     zone = "France"
     whaddup = col
     return whaddup
