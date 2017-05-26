483f09b34fcb489f65cd06693dee9069072ea1ba
argideritzalpea <crh2ke@virginia.edu>
2017-05-26 11:14:39 -0400

    Update app.py

--- app.py
+++ app.py
@@ -46,7 +46,7 @@ def makeWebhookResult(req):
     zone = parameters.get("country")
     characteristic = parameters.get("attribute")
 
-    speech = "The cost of shipping to " + zone + " is " + db.factbook.distinct(zone)[0]["birthRate.txt"] + " euros." + characteristic
+    speech = "The cost of shipping to " + zone + " is " + str(db.factbook.distinct(zone)[0]["birthRate.txt"]) + " euros." + characteristic
 
     print("Response:")
     print(speech)
