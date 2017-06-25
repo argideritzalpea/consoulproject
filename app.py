eda9391fbcb9c7464090d69918ece29a62f1da74
argideritzalpea <crh2ke@virginia.edu>
2017-06-25 16:51:06 -0700

    Update app.py

--- app.py
+++ app.py
@@ -51,7 +51,7 @@ def makeWebhookResult(req):
         parameters = result.get("parameters")
         countryBI = parameters.get("countryBI")
         bilateralcat = parameters.get("bilateralcat")
-        speech = db.bilateralcollection.distinct(countryBI)[0][bilateralcat][0]
+        speech = db.bilateralcollection.distinct(countryBI)
     
     elif req.get("result").get("action") == "compare":
         result = req.get("result")
